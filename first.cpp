53cd9bf0aac824dca2575acd6a0dce79587a8d7b
simee56 <pandeysimee7@gmail.com>
2025-12-31 21:56:57 +0530

    variable decalaration and definition

--- first.cpp
+++ first.cpp
@@ -3,5 +3,22 @@ using namespace std;
 
 int main(){
     cout<<"HII"<<endl<<"This si simee\n";
-    cout<<"Yayyyyyy";
-}
+    cout<<"Yayyyyyy\n";
+
+    //variable declaration and definition
+    int x = 3, y =5;
+    cout<<x+y<<endl;
+
+    float f = 2.45;
+    cout<<f<<endl;
+
+    char c = 'a';
+    cout<<c<<endl;
+
+    double d = 4.6666;
+    cout<<d<<endl;
+
+    bool b = true;
+    cout<<b<<endl;
+
+};
