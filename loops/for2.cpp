5e1bc04821752147b64b95a638a40211c81fe66d
simee56 <pandeysimee7@gmail.com>
2026-01-05 20:42:26 +0530

    prime number

--- loops/for2.cpp
+++ loops/for2.cpp
@@ -18,3 +18,31 @@ int main() {
         cout<<"Factorial of number "<<n<<" is "<< fact;
     }
 }
+
+
+//prime number
+#include<iostream>
+using namespace std;
+
+int main() {
+    int n ;
+    cout<<"Enter the value of n = ";
+    cin>>n;
+
+    if(n<2)
+    {
+        cout<<"Given number isnt prime.";
+        return 0;
+    }
+
+    else{
+        for(int i = 2; i < n; i++){
+            if(n%i == 0)
+            {
+                cout<<"Given number isnt prime.";
+                return 0;
+            }
+        }
+        cout<<"Given number is a prime number.";
+    }
+}
