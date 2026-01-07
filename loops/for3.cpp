597e58af845ff3d6dbbf51ab9a11b7dd0608db2b
simee56 <pandeysimee7@gmail.com>
2026-01-07 20:10:42 +0530

    Pattern printing-3

--- loops/for3.cpp
+++ loops/for3.cpp
@@ -39,3 +39,23 @@ int main()
         cout<<endl;
     }
 }
+
+
+// print -  1 2 3 4 5
+//          1 2 3 4 5
+//          1 2 3 4 5
+//          1 2 3 4 5
+//          1 2 3 4 5
+#include<iostream>
+using namespace std;
+int main() 
+{
+    for (int j = 1; j <= 5; j++)
+    {
+        for(int i = 1; i <= 5; i++)
+        {
+            cout<<i<<" ";
+        }
+        cout<<endl;
+    }
+}
