49bb9c2559b210fbe6d277048f8c521ce5e817e5
simee56 <pandeysimee7@gmail.com>
2026-01-07 21:08:48 +0530

    Pattern printing-6

--- loops/for3.cpp
+++ loops/for3.cpp
@@ -81,11 +81,11 @@ int main()
 }
 
 
-// print -  a b c d e
-//          a b c d e
-//          a b c d e
-//          a b c d e
-//          a b c d e
+// print -  a a a a a 
+//          b b b b b
+//          c c c c c
+//          d d d d d
+//          e e e e e
 #include<iostream>
 using namespace std;
 
@@ -96,6 +96,30 @@ int main()
         for(int col = 1; col <= 5; col++)
         {
             cout<<row<<" ";
+        }
+        cout<<endl;
+    }
+}
+
+
+// print - 1 2 3 4 5
+//         6 7 8 9 10
+//         11 12 13 14 15
+//         16 17 18 19 20
+//         21 22 23 24 25
+#include<iostream>
+using namespace std;
+
+int main() 
+{
+    int count = 1;
+
+    for(int j=1; j<=5; j++)
+    {
+        for(int i=1; i<=5;i++)
+        {
+            cout<<count<<" ";
+            count = count+1;
         }
         cout<<endl;
     }
