e9722a375698789ad0c296f8bb5df32a41affb24
simee56 <pandeysimee7@gmail.com>
2026-01-08 20:19:41 +0530

    Pattern printing-2(pallindrom)

--- loops/for5.cpp
+++ loops/for5.cpp
@@ -19,7 +19,34 @@ int main()
         for(int col=1; col<=2*row-1; col++)
         cout<<"* ";
 
-        
+        cout<<endl;
+    }
+}
+
+
+//print - 1
+//         1 2 1
+//       1 2 3 2 1
+//     1 2 3 4 3 2 1
+//   1 2 3 4 5 4 3 2 1
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n, row, col;
+    cout<<"Enter the value of n = ";
+    cin>>n;
+
+    for(row=1; row<=n; row++)
+    {
+        for(col=1 ; col<=n-row; col++)
+        cout<<"  ";
+
+        for(col=1; col<=row; col++)
+        cout<<col<<" ";
+
+        for(col=row-1; col>=1;col--)
+        cout<<col<<" ";
 
         cout<<endl;
     }
