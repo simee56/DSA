9f2fe6f3f506cc68a10aa3aa7a2e476a61273680
simee56 <pandeysimee7@gmail.com>
2026-01-08 18:00:32 +0530

    Pattern printing-2

--- loops/for4.cpp
+++ loops/for4.cpp
@@ -28,3 +28,32 @@ int main()
         cout<<endl;
     }
 }
+
+
+//print -    1
+//         1 2 
+//       1 2 3
+//     1 2 3 4
+//   1 2 3 4 5
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n, row, col;
+    cout<<"Enter the value of n = ";
+    cin>>n;
+
+    for(row=1; row<=n; row++)
+    {
+        //print space
+        for(col=1;col<=n-row;col++)
+        cout<<"  ";
+
+        //print numbers
+        for(col=1;col<=row;col++)
+        cout<<col<<" ";
+
+        cout<<endl;
+
+    }
+}
