cc24fa391a60da5e04a5aefc232ce8d4ac390805
simee56 <pandeysimee7@gmail.com>
2026-01-08 19:28:13 +0530

    Pattern printing-1

---
+++ loops/for5.cpp
@@ -0,0 +1,27 @@
+//print patterns for n
+
+// print -  *
+//        * * * 
+//      * * * * * 
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout<<"Enter the value of n = ";
+    cin>>n;
+
+    for(int row=1; row<=n; row++)
+    {
+        for(int col=1; col<=n-row; col++)
+        cout<<"  ";
+
+        for(int col=1; col<=2*row-1; col++)
+        cout<<"* ";
+
+        for(int col=1; col<=n-row; col++)
+        cout<<"  ";
+
+        cout<<endl;
+    }
+}
