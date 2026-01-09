f79317971ee14c4c20bca42d24e1f31c06fc5468
simee56 <pandeysimee7@gmail.com>
2026-01-09 13:38:13 +0530

    Pattern printing-4

--- loops/for5.cpp
+++ loops/for5.cpp
@@ -98,3 +98,48 @@ int main()
         cout<<endl;
     }
 }
+
+
+// print - Butterfly kinda pattern
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n, row, col;
+    cout<<"Enter the value of n = ";
+    cin>>n;
+
+    for(row=1; row<=n;row++)
+    {
+        //print star
+        for(col=1; col<=row;col++)
+        cout<<"* ";
+
+        //print space
+        for(col=1; col<=2*n-2*row; col++)
+        cout<<"  ";
+
+        //print star
+        for(col=1; col<=row;col++)
+        cout<<"* ";
+
+        cout<<endl;
+    }
+
+    for(row =n-1; row>=1; row--)
+    {
+        //print star
+        for(col=1; col<=row;col++)
+        cout<<"* ";
+
+        //print space
+        for(col=1; col<=2*n-2*row; col++)
+        cout<<"  ";
+
+        //print star
+        for(col=1; col<=row;col++)
+        cout<<"* ";
+
+        cout<<endl;
+    }
+}
