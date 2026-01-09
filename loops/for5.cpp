5d80dd75ca2a2e7c0b5f799954b1117caf04a824
simee56 <pandeysimee7@gmail.com>
2026-01-09 14:23:44 +0530

    pattern printing-5

--- loops/for5.cpp
+++ loops/for5.cpp
@@ -143,3 +143,41 @@ int main()
         cout<<endl;
     }
 }
+
+
+
+//print diamond kinda pattern
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n, row, col;
+    cout<<"Enter the value of n = ";
+    cin>>n;
+
+    for(row=1; row<=n; row++)
+    {
+        //print space
+        for(col=1; col<=n-row; col++)
+        cout<<" ";
+
+        //print star
+        for(col=1; col<=row; col++)
+        cout<<"* ";
+
+        cout<<endl;
+    }
+
+    for(row=n; row>=1; row--)
+    {
+        //print space
+        for(col=1; col<=n-row; col++)
+        cout<<" ";
+
+        //print star
+        for(col=1; col<=row; col++)
+        cout<<"* ";
+
+        cout<<endl;
+    }
+}
