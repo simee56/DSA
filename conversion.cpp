b398655c2275090149decad425fdeda7fd0a24b1
simee56 <pandeysimee7@gmail.com>
2026-01-11 18:52:44 +0530

    Conversion from Decimal to Binary

--- conversion.cpp
+++ conversion.cpp
@@ -1,3 +1,4 @@
+//Decimal to Binary
 #include<iostream>
 using namespace std;
 int main()
@@ -22,4 +23,60 @@ int main()
         mul = mul*10;
     }
     cout<<"The binary form of given number is = "<<ans;
+}
+
+
+//Binary to Decimal
+#include<iostream>
+using namespace std;
+int main()
+{
+    int num;
+    cout<<"Enter the number = ";
+    cin>>num;
+    int rem, ans = 0, mul = 1;
+
+    while(num > 0)
+    {
+        //remainder
+        rem = num%10;
+
+        //quotient
+        num = num/10;
+
+        //answer
+        ans = rem*mul+ans;
+
+        //multiplication
+        mul = mul*2;
+    }
+    cout<<"The Decimal form of given number is = "<<ans;
+}
+
+
+//Binary to Decimal
+#include<iostream>
+using namespace std;
+int main()
+{
+    int num;
+    cout<<"Enter the number = ";
+    cin>>num;
+    int rem, ans = 0, mul = 1;
+
+    while(num > 0)
+    {
+        //remainder
+        rem = num%8;
+
+        //quotient
+        num = num/8;
+
+        //answer
+        ans = rem*mul+ans;
+
+        //multiplication
+        mul = mul*8;
+    }
+    cout<<"The Decimal form of given number is = "<<ans;
 }
