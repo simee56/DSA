f69ebea9208729c2c45642a09c68e73a2e3ff977
simee56 <pandeysimee7@gmail.com>
2026-01-12 12:32:28 +0530

    Leap year

---
+++ extra.cpp
@@ -0,0 +1,18 @@
+//Leap Year
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout<<"Enter the year = ";
+    cin>>n;
+
+    if(n%400 == 0)
+    cout<<"Given year "<<n<<" is a Leap Year.";
+
+    else if(n%4 == 0 && n%100 !=0)
+    cout<<"Given year "<<n<<" is a Leap Year.";
+
+    else
+    cout<<"Given year "<<n<<" is not a Leap Year.";
+}
