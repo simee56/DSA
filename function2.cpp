162b3853678e3f16503ff7e55489d78ae38e92c8
simee56 <pandeysimee7@gmail.com>
2026-01-14 23:00:36 +0530

    Armstrong number

--- function2.cpp
+++ function2.cpp
@@ -18,3 +18,51 @@ int main()
     char ans = Convert(character);
     cout<<"Given character "<<character<<" is converted into "<<ans;
 }
+
+
+//Armstrong number
+#include<iostream>
+#include<cmath>
+using namespace std;
+
+int CountDigit(int num)
+{
+    int count = 0;
+    while(num)
+    {
+        count++;
+        num/=10;
+    }
+
+    return count;
+}
+
+bool isArmstrong(int num, int digits)
+{
+    int n= num, rem, ans = 0;
+        while(n)
+    {
+        rem = n % 10;
+
+        int power = 1;
+        for(int i = 0; i < digits; i++)
+        {
+            power *= rem;
+        }
+
+        ans += power;
+        n /= 10;
+    }
+
+    return ans == num;
+}
+
+
+int main()
+{
+    int num;
+    cin>>num;
+    int digits = CountDigit(num);
+    int ans = isArmstrong(num, digits);
+    cout<<ans;
+}
