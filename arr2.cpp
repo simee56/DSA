9b0cd707df64f3a3b9abdb315cbe6e8a999c3889
simee56 <pandeysimee7@gmail.com>
2026-01-16 00:09:40 +0530

    search element using function

--- arr2.cpp
+++ arr2.cpp
@@ -26,3 +26,37 @@ int main()
 
      cout<<"search element is not present in the array.";
 }
+
+
+//using function
+#include<iostream>
+using namespace std;
+
+int SeachElement(int arr[10])
+{
+    int x;
+    cout<<"Enter the search element = ";
+    cin>>x;
+
+    for(int i =0; i<10; i++)
+    {
+        if(arr[i] == x)
+        return i;
+    }
+
+    return -1;
+}
+
+int main()
+{
+    int arr[10];
+    cout<<"Enter the elements of array = ";
+
+    for(int i =0; i<10; i++)
+    {
+        cin>>arr[i];
+    }
+
+    cout<<SeachElement(arr);
+}
+
