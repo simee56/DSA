ec2f29123bf3d2ae8ef54446f683aba0a44a67f8
simee56 <pandeysimee7@gmail.com>
2026-01-20 21:23:29 +0530

    Selection sort using function

--- sorting/selection.cpp
+++ sorting/selection.cpp
@@ -29,3 +29,48 @@ int main()
         cout<<arr[i]<<" ";
     }
 }
+
+
+
+
+//USING FUNCTIONS
+#include<iostream>
+using namespace std;
+
+void SelectionSort(int arr[], int n)
+{
+    for(int i =0; i<n-1; i++)
+    {
+       int index = i;
+
+        for(int j =i+1; j<n; j++)
+        {
+            if(arr[j] < arr[index])
+            index = j;
+        }
+
+        swap(arr[index], arr[i]);
+    };
+
+}
+
+
+int main()
+{
+    int arr[1000];
+    int n;
+    cout<<"Enter the size of an array : ";
+    cin>>n;
+
+    cout<<"Enter the elements pf an array : ";
+    for(int i = 0; i<n; i++)
+    cin>>arr[i];
+
+    SelectionSort(arr, n);
+
+    cout<<"Sorted Array : ";
+    for(int i = 0; i<n; i++)
+    {
+        cout<<arr[i]<<" ";
+    }
+}
