13431e64b0b617be0fc3e35c68a804fce0894273
simee56 <pandeysimee7@gmail.com>
2026-01-23 11:31:18 +0530

    Linear Search algo

---
+++ searching/linear.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int arr[1000];
+    int n;
+    cout<<"Enter the number present in array = ";
+    cin>>n;
+
+    cout<<"Enter the elemetns of the array  = ";
+    for(int i = 0; i < n; i++)
+    cin>>arr[i];
+
+    int x;
+    cout<<"Enter the searching element = ";
+    cin>>x;
+
+    bool found = false;
+
+    for(int i = 0; i<n; i++)
+    {
+        if(arr[i] == x)
+        { 
+        cout<<"element "<<x<<" found at the index "<<i;
+        found = true;
+        break;
+        }
+    }
+
+    if(!found)
+    cout<<"element "<<x<<" is not present at any index ";
+}
