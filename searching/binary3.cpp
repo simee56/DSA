b3e9edb60feedf0acf759c26830ee88db7ea2fe5
simee56 <pandeysimee7@gmail.com>
2026-01-29 23:06:36 +0530

    Painter partition problem

--- searching/binary3.cpp
+++ searching/binary3.cpp
@@ -61,3 +61,68 @@ int main()
 
     cout<<AllocateBook(arr, n, M);
 }
+
+
+//Painter partition 
+#include<iostream>
+using namespace std;
+
+int MinTime(int arr[], int n, int M)
+{
+    if(M >n)
+    return -1;
+
+    int start = 0, end =0, mid, ans;
+    for(int i =0; i<n; i++)
+    {
+        start = max(start, arr[i]);
+        end += arr[i];
+    };
+
+    while(start<=end)
+    {
+        mid = start + (end- start) / 2;
+
+        int walls =0, count =1;
+
+        for(int i =0; i<n; i++)
+        {
+            walls += arr[i];
+
+            if(walls > mid)
+            {
+                count++;
+                walls = arr[i];
+            }
+        }
+
+        if(count <= M)
+        {
+            ans = mid;
+            end = mid-1;
+        }
+
+        else
+        start = mid+1;
+    }
+
+    return ans;
+}
+
+int main()
+{
+    int arr[1000];
+    int n;
+    cout<<"Enter the size of the array : ";
+    cin>>n;
+
+    cout<<"Enter the elements of the array : ";
+    for(int i = 0; i<n; i++)
+    cin>>arr[i];
+
+    int M ;
+    cout<<"Enter the value of M(Painters) : ";
+    cin>>M;
+
+    cout<<MinTime(arr, n, M);
+}
