8c2a5976d45e28289f1dc3ee7b6c5d3d42d44350
simee56 <pandeysimee7@gmail.com>
2026-01-30 13:21:29 +0530

    Aggressive Cow Problem

--- searching/binary3.cpp
+++ searching/binary3.cpp
@@ -192,3 +192,59 @@ int main()
     cout<<MinWeight(arr, n, M);
 }
 
+
+
+//Aggresive cows
+#include<iostream>
+using namespace std;
+
+int MinDistance(int arr[], int N, int M)
+{
+    int start = 1, end = arr[N-1] - arr[0], mid, ans;
+
+    while(start <= end)
+    {
+        mid = start + (end - start) / 2;
+
+        int count = 1, pos = arr[0];
+
+        for(int i = 1; i<N; i++)
+        {
+            if(pos + mid <= arr[i])
+            {
+                count++;
+                pos = arr[i];
+            }
+        }
+
+        if(count < M)
+        end = mid-1;
+        
+        else
+        {
+            ans = mid;
+            start = mid + 1;
+        }
+    }
+
+    return ans;
+}
+
+int main()
+{
+    int arr[1000];
+    int n;
+    cout<<"Enter the size of the array : ";
+    cin>>n;
+
+    cout<<"Enter the elements of the array : ";
+    for(int i = 0; i<n; i++)
+    cin>>arr[i];
+
+    int M ;
+    cout<<"Enter the value of M(Cows) : ";
+    cin>>M;
+
+    cout<<MinDistance(arr, n, M);
+}
+
