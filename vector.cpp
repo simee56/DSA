1239c80eafaa4fc492df71655b553bec040156e3
simee56 <pandeysimee7@gmail.com>
2026-02-02 19:18:39 +0530

    Create a vector

---
+++ vector.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int main()
+{
+    //create vector, declare
+    vector<int>v;
+    //size and capacity
+    cout<<"Size of the vecotr is "<<v.size()<<endl;
+    cout<<"Capacity of the vecotr is "<<v.capacity()<<endl;
+
+}
