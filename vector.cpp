62f7ee241565c1aa1863ab901c2e68619942ca6e
simee56 <pandeysimee7@gmail.com>
2026-02-02 19:19:57 +0530

    push_back element

--- vector.cpp
+++ vector.cpp
@@ -4,9 +4,13 @@ using namespace std;
 
 int main()
 {
-    //create vector, declare
+    //Create vector, declare
     vector<int>v;
     //size and capacity
+    cout<<"Size of the vecotr is "<<v.size()<<endl;
+    cout<<"Capacity of the vecotr is "<<v.capacity()<<endl;
+
+    v.push_back(2);
     cout<<"Size of the vecotr is "<<v.size()<<endl;
     cout<<"Capacity of the vecotr is "<<v.capacity()<<endl;
 
