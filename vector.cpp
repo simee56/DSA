997562a7d7c5f3c4725dc33ff425697301583633
simee56 <pandeysimee7@gmail.com>
2026-02-02 19:22:17 +0530

    update value

--- vector.cpp
+++ vector.cpp
@@ -6,12 +6,15 @@ int main()
 {
     //Create vector, declare
     vector<int>v;
-    //size and capacity
+    //Size and capacity
     cout<<"Size of the vecotr is "<<v.size()<<endl;
     cout<<"Capacity of the vecotr is "<<v.capacity()<<endl;
 
+    //Insert elements
     v.push_back(2);
     cout<<"Size of the vecotr is "<<v.size()<<endl;
     cout<<"Capacity of the vecotr is "<<v.capacity()<<endl;
 
+    //Update value
+    v[0] = 1;
 }
