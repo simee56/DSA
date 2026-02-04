d1c581b0f75469855159c761da69ec54b0d2037a
simee56 <pandeysimee7@gmail.com>
2026-02-04 23:08:30 +0530

    Seggregate 0 an 1

---
+++ twoP.cpp
@@ -0,0 +1,42 @@
+//TWO POINTER PROBLEM - seggregate 0 and 1
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cout<<"Enter the size of an array : ";
+    cin>>n;
+
+    int arr[1000];
+    cout<<"Enter the elements of an array :";
+    for(int i =0; i<n; i++)
+    cin>>arr[i];
+
+    int start = 0, end = n-1;
+
+    for(int i = 0; i<n; i++)
+    {
+        if(arr[start] == 0)
+        start++;
+
+        else 
+        {
+            if(arr[end] == 1)
+            end--;
+
+            else
+            {
+                swap(arr[start], arr[end]);
+                end--;
+            }
+        }
+
+    }
+
+    for(int i = 0; i<n; i++)
+    {
+        cout<<arr[i];
+    }
+
+}
