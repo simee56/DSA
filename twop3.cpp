5a7f99471f32b842094c72a64f0a846968b53b41
simee56 <pandeysimee7@gmail.com>
2026-02-08 18:30:06 +0530

    Pair with given difference - using binary search

--- twop3.cpp
+++ twop3.cpp
@@ -22,15 +22,69 @@ int numbersExist(vector<int>& v, int target)
         for(int j = i+1; j<n; j++)
         {
             if(v[j] - v[i] == target)
-            {
-                found = true;
-                return 1;
-                break;
-            }
+            return 1;
         }
     }
+    return -1;
+}
 
-    if(!found)
+int main()
+{
+    int n;
+    cout<<"Enter the size : ";
+    cin>>n;
+
+    vector<int>v(n);
+    cout<<"Enter the elements : ";
+    for(int i = 0; i<n ;i++)
+    cin>>v[i];
+
+    int X;
+    cout<<"Enter target vlaue : ";
+    cin>>X;
+
+    cout<<numbersExist(v, X);
+}
+
+//Pair with given differnce - Binary Search (time complexity (o(n(logn))))
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+bool BinarySearch(vector<int> & v, int start, int end, int x)
+{
+    while(start <= end)
+    {
+        int mid = start + (end - start) /2;
+        
+        if(v[mid] == x)
+        return true;
+
+        else if(v[mid] > x)
+        end = mid - 1;
+
+        else
+        start = mid + 1;
+    }
+
+    return false;
+}
+
+int numbersExist(vector<int> & v, int target)
+{
+    int n = v.size();
+    bool found = false;
+
+    sort(v.begin(), v.end());
+
+    for(int i = 0; i < n-1; i++)
+    {
+        int x = target + v[i];
+
+        if (BinarySearch(v, i+1, n-1, x))
+        return 1;
+    }
     return -1;
 }
 
