5ef69559ad029f021bb811d858c39974385c4dfe
simee56 <pandeysimee7@gmail.com>
2026-02-09 23:35:15 +0530

    Add two matrices

--- 2D-arr2.cpp
+++ 2D-arr2.cpp
@@ -45,3 +45,41 @@ int main()
 
     SearchElement(arr, row, col, X);
 }
+
+
+//Add two matrix
+#include<iostream>
+using namespace std;
+int main()
+{
+    int row, col;
+    cout<<"Enter the value of row : ";
+    cin>>row;
+
+    cout<<"Enter the value of column : ";
+    cin>>col;
+
+    int matrix1[100][100];
+    cout<<"Enter the elemnts in matrix1 : ";
+    for(int i = 0; i < row; i++)
+    for(int j = 0; j < col; j++)
+    cin>>matrix1[i][j];
+
+    int matrix2[100][100];
+    cout<<"Enter the elemnts in matrix2 : ";
+    for(int i = 0; i < row; i++)
+    for(int j = 0; j < col; j++)
+    cin>>matrix2[i][j];
+
+    int sum[100][100];
+    for(int i = 0; i<row; i++)
+    {
+        for(int j = 0; j<col; j++)
+        sum[i][j] = matrix1[i][j] + matrix2[i][j];
+    }
+
+    cout<<"The sum matrix is : ";
+    for(int i = 0; i < row; i++)
+    for(int j = 0; j < col; j++)
+    cout<<sum[i][j]<<" ";
+}
