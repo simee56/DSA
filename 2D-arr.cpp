45786885c313619bbe8afd9401af3031ac549270
simee56 <pandeysimee7@gmail.com>
2026-02-09 22:36:45 +0530

    Print all the values of array - row wise

--- 2D-arr.cpp
+++ 2D-arr.cpp
@@ -4,4 +4,11 @@ int main()
 {
     //Create and initialize 2D array
     int arr[3][4] = {1,2,3,4,5,6,7,8,9,10,11,12};
+
+    //print all the value
+    for(int i = 0; i < 3; i++)
+    {
+        for(int j = 0; j < 4; j++)
+        cout<<arr[i][j]<<" ";
+    }
 }
