c7896e5e4692023d7eb3d15a8e8ea5db198bef5c
simee56 <pandeysimee7@gmail.com>
2026-02-14 12:47:26 +0530

    Linear search in 2D array

--- 2D-arr.cpp
+++ 2D-arr.cpp
@@ -26,3 +26,51 @@ int main()
     //print all the values column wise - using function call
     printcolwise(arr, 4, 3);
 }
+
+//Search an element in 2D array - Linear search
+#include<iostream>
+using namespace std;
+
+void SearchElement(int arr[][100], int row, int col, int X)
+{
+    bool found = false;
+
+    for(int i = 0; i<row; i++)
+    {
+        for(int j = 0 ; j<col; j++)
+        {
+            if(arr[i][j] == X)
+            {
+                cout<<"value "<<X<<" is found at position ("<< i <<", "<< j << ")";
+                found = true;
+                return;
+            }            
+        }
+    }
+    if(!found)
+    cout<<"value "<<X<<" is not found.";
+};
+
+int main()
+{
+    int row, col;
+    cout<<"Enter the value of row : ";
+    cin>>row;
+
+    cout<<"Enter the value of column : ";
+    cin>>col;
+
+    int arr[100][100];
+
+    cout<<"Enter the elemnts in array : ";
+
+    for(int i = 0; i < row; i++)
+    for(int j = 0; j < col; j++)
+    cin>>arr[i][j];
+
+    int X;
+    cout<<"Enter the value to search : ";
+    cin>>X;
+
+    SearchElement(arr, row, col, X);
+}
