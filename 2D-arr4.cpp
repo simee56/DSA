c0c8f4824943060eeaba9b9904cb50c4c65e9c97
simee56 <pandeysimee7@gmail.com>
2026-02-20 19:55:02 +0530

    Changes

--- 2D-arr4.cpp
+++ 2D-arr4.cpp
@@ -43,3 +43,33 @@ int main()
     cout<<mat[i][j]<<" ";
 
 }
+
+
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n, m;
+    cout<<"Enter the number of rows and cols : ";
+    cin>>n>>m;
+
+    int arr[100][100];
+    
+    for(int i = 0; i<n; i++)
+    for(int j = 0; j<m; j++)
+    cin>>arr[i][j];
+
+
+    for(int j = 0; j<m; j++)
+    {    
+        int sum = 0;
+
+        cout<<"\nSUm of col "<<j<<" is : \n";
+        for(int i = 0; i<n; i++)
+        {
+            sum =  sum+ arr[i][j];
+        }
+        cout<<sum;
+    }
+}
