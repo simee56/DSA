daa43fa6dd7bae063fb979b6d2ac6e2476f65b9b
simee56 <pandeysimee7@gmail.com>
2026-03-31 17:26:53 +0530

    use cin.ignore()

--- string1.cpp
+++ string1.cpp
@@ -2,12 +2,14 @@
 using namespace std;
 int main()
 {
+    //Normal method - use cin but ts doesnt print space. 
     string s;
     cout<<"Enter the string : ";
     cin>>s;
     cout<<"The output is : "<<s;
 
 
+    //use getline and cin.ignore() - getline prints space too and cin.ignore() helps getline to not to read the empty string.
     string news;
     cout<<"\nEnter the second string : ";
     cin.ignore();
