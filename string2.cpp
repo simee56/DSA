7c908af3f71e96f4456b2b88309007d62d693314
simee56 <pandeysimee7@gmail.com>
2026-03-31 17:59:26 +0530

    Pallindorme

--- string2.cpp
+++ string2.cpp
@@ -36,3 +36,28 @@ int main()
 
     cout<<Size;
 }
+
+
+//Pallindrome
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    string s1;
+    cin>>s1;
+
+    int start = 0, end = s1.size()-1;
+
+    while(start < end)
+    {
+        if(s1[start] != s1[end])
+        {
+            cout<<"not a Pallindrome";
+            break;
+        }
+        start++;
+        end--;
+       
+    }
+}
