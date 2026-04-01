3bf0cadf9ba4aeb3e9f24fb79450cf5b0419bad7
simee56 <pandeysimee7@gmail.com>
2026-04-01 12:55:44 +0530

    A string is rotated by two places

--- string3.cpp
+++ string3.cpp
@@ -18,6 +18,71 @@ int main()
         
         i++;
     }
-    
-    
+}
+
+
+
+//check if strign is rotated by two palces
+#include<iostream>
+using namespace std;
+
+void clockRotated(string &s)
+{
+    char c = s[0];
+    int index = 1;
+
+    while(index <= s.size())
+    {
+        s[index-1] = s[index];
+        index++;
+    }
+    s[s.size()-1] = c;
+}
+
+void antiClockRotated(string &s)
+{
+    char c = s[s.size()-1];
+    int index = s.size()-2;
+
+    while(index >= 0)
+    {
+        s[index+1] = s[index];
+        index--;
+    }
+    s[0] = c;
+}
+
+bool isRotated(string str1, string str2)
+{
+    if(str1.size() != str2.size())
+    return 0;
+
+    string clockwise, antiClockwise;
+
+    clockwise = str1;
+    clockRotated(clockwise);
+    clockRotated(clockwise);
+
+    if(clockwise == str2)
+    return 1;
+
+    antiClockwise = str1;
+    antiClockRotated(antiClockwise);
+    antiClockRotated(antiClockwise);
+
+    if(antiClockwise == str2)
+    return 1;
+
+    return 0;
+}
+
+int main()
+{
+    string str1, str2;
+    cout<<"Enter the string 1: ";
+    cin>>str1;
+    cout<<"Enter the string 1: ";
+    cin>>str2;
+
+    cout<<isRotated(str1, str2);
 }
