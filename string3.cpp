7c7ceda3acc6f61d764ac70f6760f2b4b1f55aba
simee56 <pandeysimee7@gmail.com>
2026-04-01 13:36:15 +0530

    Sort a string

--- string3.cpp
+++ string3.cpp
@@ -86,3 +86,41 @@ int main()
 
     cout<<isRotated(str1, str2);
 }
+
+
+//Sort a String
+#include<iostream>
+#include<vector>
+using namespace std;
+
+string sort(string s)
+{
+    vector<int>alpha(26,0);
+    for(int i = 0; i < s.size(); i++)
+    {
+        alpha[s[i]-'a']++;
+    }
+
+    string ans;
+    for(int i = 0; i < 26; i++)
+    {
+        char c = 'a' + i;
+        while(alpha[i])
+        {
+            ans += c;
+            alpha[i]--;
+        }
+    }
+
+    return ans;
+}
+
+int main()
+{
+    string s;
+    cout<<"Enter the string in lowercase : ";
+    cin>>s;
+
+    cout<<sort(s);
+
+}
