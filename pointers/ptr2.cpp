a503651c3cedfc90a53d3baad3755b167ded033d
simee56 <pandeysimee7@gmail.com>
2026-04-29 00:13:19 +0530

    Swapping using refernce variable

--- pointers/ptr2.cpp
+++ pointers/ptr2.cpp
@@ -41,7 +41,7 @@ int main()
     cout<<arr[i];
 }
 
-
+//SWapping using pointers
 #include<iostream>
 using namespace std;
 
@@ -57,5 +57,25 @@ int main()
 {
     int a = 9, b = 7;
     swapping(&a, &b);
-    cout<<a<<b;
+    cout<<a<<" "<<b;
 }
+
+//SWapping using Reference variable
+#include<iostream>
+using namespace std;
+
+void swapping(int &p1, int &p2)
+{
+    int temp = p1;
+    p1 = p2;
+    p2 = temp;
+
+}
+
+int main()
+{
+    int a = 9, b = 7;
+    swapping(a, b);
+    cout<<a<<" "<<b;
+}
+
