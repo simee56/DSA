81d80e4b98364f4c9b03c9959d67cdd25855dfff
simee56 <pandeysimee7@gmail.com>
2026-04-28 23:52:30 +0530

    Swapping using pointer

--- pointers/ptr2.cpp
+++ pointers/ptr2.cpp
@@ -22,3 +22,40 @@ int main()
 }
 
 
+
+
+#include<iostream>
+using namespace std;
+
+void d(int *p)
+{
+    for(int i = 0; i<n; i++)
+    p[i]= p[i]*2;
+}
+
+int main()
+{
+    int arr[5] = {1,2,3,4,5};
+    d(arr, 5);
+    for(int i  =0; i<arr.size(); i++)
+    cout<<arr[i];
+}
+
+
+#include<iostream>
+using namespace std;
+
+void swap(int *p1, int *p2)
+{
+    int temp = *p1;
+    *p1 = *p2;
+    *p2 = temp;
+
+}
+
+int main()
+{
+    int a = 9, b = 7;
+    swap(&a, &b);
+    cout<<a<<b;
+}
