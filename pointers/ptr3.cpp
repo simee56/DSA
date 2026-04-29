fc50b2743ad0b179fa82b7a80ffc0f4941fa020e
simee56 <pandeysimee7@gmail.com>
2026-04-29 22:37:28 +0530

    update the value of n usin p1 and p2

--- pointers/ptr3.cpp
+++ pointers/ptr3.cpp
@@ -6,8 +6,17 @@ using namespace std;
 int main()
 {
     int n = 10;
-    int *p1 = &n;
+
+    int *p1 = &n;    //Stores the address of variable n
     cout<<p1<<endl;
-    int **p2 = &p1;
+
+    int **p2 = &p1;  //Stores the address of pointer P1
     cout<<p2<<endl;
+
+    *p1 = *p1 +10;  //Updating the value of n using p1
+    cout<<n<<endl;
+
+    **p2 = **p2 + 1;   //Updating the value of n using p2
+    cout<<**p2;
+
 }
