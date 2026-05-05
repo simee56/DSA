c13f3138e6d4623cb2e8d117a7721edc705ca5fe
simee56 <pandeysimee7@gmail.com>
2026-05-05 12:59:25 +0530

    3D array creation using pointers

--- pointers/ptr4.cpp
+++ pointers/ptr4.cpp
@@ -46,3 +46,48 @@ int main()
 
     delete [] ptr;  //This will release the memory of address array
 }
+
+
+
+
+//3D array creation using pointers
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int x, y, z;
+    cout<<"Enter the values of x, y and z : ";
+    cin>>x>>y>>z;
+
+    //Firstly create the array which will store the addresess of the 2d arrays
+    int ***ptr  = new int **[x];
+
+    //Now creating 2 Array
+    for(int i = 0; i < x; i++)
+    {
+        ptr[i] = new int *[y];
+        for(int j = 0; j < y; j++)
+        {
+            ptr[i][j] = new int [z];
+        }
+    }
+
+    for(int i = 0; i< x; i++)
+    {
+        for(int j =0; j <y; j++)
+        {
+            for(int k =0; k < z; k++)
+            ptr[i][j][k] = i+j+k;
+        }
+    }
+
+     for(int i = 0; i< x; i++)
+    {
+        for(int j =0; j <y; j++)
+        {
+            for(int k =0; k < z; k++)
+            cout<<ptr[i][j][k]<<" ";
+        }
+    }
+}
