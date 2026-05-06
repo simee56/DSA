b72dc160ba4246551e35ace9ec40f14356c136a5
simee56 <pandeysimee7@gmail.com>
2026-05-06 15:06:52 +0530

    odd numberws from 1 to n

--- Recursion/rec1.cpp
+++ Recursion/rec1.cpp
@@ -83,3 +83,37 @@ int main()
 }
 
 
+
+//print odd numbers from 1 to n
+#include<iostream>
+using namespace std;
+
+void print(int n)
+{
+    if(n == 1)
+    {
+        cout<<n;
+        return;
+    }
+
+    print(n - 2);
+    cout<<"\n"<<n;
+}
+
+
+int main()
+{
+    int n;
+    cout<<"Enter the value of n : ";
+    cin>>n;
+
+    if(n%2 == 1)
+    print(n);
+
+    else
+    {
+        n = n-1;
+        print(n);
+    }
+
+}
