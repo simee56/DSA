ea5fc33d12057e71020c2c1287543454df08ab41
simee56 <pandeysimee7@gmail.com>
2026-05-08 13:13:37 +0530

    Sum of n natural numbers

--- Recursion/rec2.cpp
+++ Recursion/rec2.cpp
@@ -1,10 +1,9 @@
+//Factorial of a number
 #include<iostream>
 using namespace std;
 
 int factorial(int n)
 {
-    int fact;
-
     //Base Case
     if(n==1 || n==0)
     return 1;
@@ -25,4 +24,33 @@ int main()
         return 0;
     }
     cout<<"Factorial of given number "<<n<<" is : "<<factorial(n);
+}
+
+
+//Sum of n natural numbers
+#include<iostream>
+using namespace std;
+
+int sumOfNums(int n)
+{
+    //Base Case
+    if(n==1 )
+    return 1;
+
+    return n + sumOfNums(n-1);
+
+}
+
+int main()
+{
+    int n;
+    cout<<"Enter the number : ";
+    cin>>n;
+
+    if( n < 0)
+    {
+        cout<<"Sum of negative number isn't possible :(";
+        return 0;
+    }
+    cout<<"Sum of given number "<<n<<" is : "<<sumOfNums(n);
 }
