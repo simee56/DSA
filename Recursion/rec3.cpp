3211780ad5c057dfe27cfc2ce5f75febfd2a17d8
simee56 <pandeysimee7@gmail.com>
2026-05-09 22:54:07 +0530

    Print the elements if the indx starts from n-1

--- Recursion/rec3.cpp
+++ Recursion/rec3.cpp
@@ -54,3 +54,37 @@ int main()
     printArr(arr, 0, n);
 }
 
+
+
+
+//Print the elements of an array if index starts with n-1
+#include<iostream>
+using namespace std;
+
+void printArr(int arr[], int index, int n)
+{
+    if(index < 0)
+    return;
+
+    cout<<arr[index]<<" ";
+    printArr(arr, index - 1, n);
+
+}
+
+int main()
+{
+    int arr[100];
+    int n;
+    cout<<"Enter the size : ";
+    cin>>n;
+
+    cout<<"Enter the elements : ";
+    for(int i = 0; i<n; i++)
+    cin>>arr[i];
+
+    printArr(arr, n-1, n);
+}
+
+
+
+
