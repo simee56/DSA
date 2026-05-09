1f87b3aa1000ba346dc446ae0126e9103ff17753
simee56 <pandeysimee7@gmail.com>
2026-05-09 22:51:32 +0530

    PRint elements in reverse order

--- Recursion/rec3.cpp
+++ Recursion/rec3.cpp
@@ -24,3 +24,33 @@ int main()
 
     printArr(arr, 0, n);
 }
+
+
+
+//Print the elements of an array in reverse order if index starts with 0
+#include<iostream>
+using namespace std;
+
+void printArr(int arr[], int index, int n)
+{
+    if(index == n)
+    return;
+
+    printArr(arr, index + 1, n);
+    cout<<arr[index]<<" ";
+}
+
+int main()
+{
+    int arr[100];
+    int n;
+    cout<<"Enter the size : ";
+    cin>>n;
+
+    cout<<"Enter the elements : ";
+    for(int i = 0; i<n; i++)
+    cin>>arr[i];
+
+    printArr(arr, 0, n);
+}
+
