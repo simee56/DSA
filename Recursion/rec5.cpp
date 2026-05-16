121e44781130ee60ac71637e11c9570c82103395
simee56 <pandeysimee7@gmail.com>
2026-05-16 22:39:30 +0530

    Binary Search

--- Recursion/rec5.cpp
+++ Recursion/rec5.cpp
@@ -32,3 +32,46 @@ int main()
 
     cout<<linearSearch(arr, X, index, n);
 }
+
+
+//Binary search
+#include<iostream>
+using namespace std;
+
+bool binarySearch(int arr[], int X, int start, int end)
+{
+    if(start > end)
+    return -1;
+
+    int mid =  start + (end - start) / 2;
+
+    if(arr[mid] == X)
+    return 1;
+
+    else if(arr[mid] < X)
+    return binarySearch(arr, X, mid + 1, end);
+
+    else
+    return binarySearch(arr, X, start, mid + 1);
+
+}
+
+int main()
+{
+    int arr[100];
+    int n;
+    cout<<"Enter the size of array : ";
+    cin>>n;
+
+    cout<<"Enter array's elements are in sorted manner: ";
+    for(int i = 0; i < n; i++)
+    cin>>arr[i];
+
+    int X;
+    cout<<"Enter the search element : ";
+    cin>>X;
+
+    int start = 0, end = n-1;
+
+    cout<<binarySearch(arr, X, start, end);
+}
