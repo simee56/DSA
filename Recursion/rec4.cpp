298574015cc533eb46bb816764f2abe930259734
simee56 <pandeysimee7@gmail.com>
2026-05-16 21:36:40 +0530

    Reverse a String

--- Recursion/rec4.cpp
+++ Recursion/rec4.cpp
@@ -49,3 +49,31 @@ int main()
 
     cout<<countVowels(str, index);
 };
+
+
+//Reverse a string
+#include<iostream>
+using namespace std;
+
+void reverseString(string &str, int start, int end)
+{
+    if(start >= end)
+    return;
+
+    char c = str[start];
+    str[start] =  str[end];
+    str[end] = c;
+
+    reverseString(str, start + 1, end -1);
+}
+
+int main()
+{
+    string str;
+    cin>>str;
+
+    int start = 0, end = str.size() -1;
+
+    reverseString(str, start, end);
+    cout<<str;
+}
