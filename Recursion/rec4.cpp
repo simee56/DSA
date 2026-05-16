4c073445f23a8a87dedcee20fe89978e461f4664
simee56 <pandeysimee7@gmail.com>
2026-05-16 21:24:30 +0530

    Count Vowels

--- Recursion/rec4.cpp
+++ Recursion/rec4.cpp
@@ -22,3 +22,30 @@ int main()
     
     cout<<checkPallindrome(str, start, end);
 }
+
+
+//Count vowels
+#include<iostream>
+using namespace std;
+
+int countVowels(string str, int index)
+{
+    if(index == -1)
+    return 0;
+
+    if(str[index] == 'a' || str[index] == 'e' || str[index] == 'i' || str[index] == 'o' || str[index] == 'u')
+    return 1 + countVowels(str, index - 1);
+
+    else 
+    return countVowels(str, index - 1);
+}
+
+int main()
+{
+    string str;
+    cin>>str;
+
+    int index = str.size()-1;
+
+    cout<<countVowels(str, index);
+};
