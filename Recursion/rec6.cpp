c97ee6fe1c98350fb51ef6ece0405bcdb93b27c2
simee56 <pandeysimee7@gmail.com>
2026-06-07 19:39:30 +0530

    Print subset of a string

--- Recursion/rec6.cpp
+++ Recursion/rec6.cpp
@@ -47,6 +47,55 @@ int main()
         }
         cout<<endl;
     }
+}
 
-    
+//Print all the subets of a String
+#include<iostream>
+#include<algorithm>
+#include<vector>
+using namespace std;
+
+void subseq(vector<string>arr, int index, int n, vector<string>temp, vector<vector<string> >&ans)
+{
+
+    if(index == n)
+    {
+        ans.push_back(temp);
+        return;
+    }
+
+    //Element isnt included in the set
+    subseq(arr, index+1, n, temp, ans);
+
+    //Element is included in the set
+    temp.push_back(arr[index]);
+    subseq(arr, index+1, n, temp, ans);
+
+
+}
+
+int main()
+{
+    int n;
+    cout<<"Enter the value of n : ";
+    cin>>n; 
+
+    vector<string>arr(n);
+    cout<<"Enter the elements of the array : ";
+    for(int i = 0; i<n ; i++)
+    cin>>arr[i];
+
+    vector<string>temp;
+    vector<vector<string> >ans;
+
+    subseq(arr, 0, n, temp, ans);
+
+    for(int i =0; i<ans.size(); i++)
+    {
+        for(int j =0; j<ans[i].size(); j++)
+        {
+            cout<<ans[i][j]<<" ";
+        }
+        cout<<endl;
+    }
 }
