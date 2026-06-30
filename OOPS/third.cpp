ee68e8644bb99fa756623d301a2c2bcabfe4a9ae
simee56 <pandeysimee7@gmail.com>
2026-06-30 14:46:38 +0530

    USe of static data member to have the count of totalBalance

--- OOPS/third.cpp
+++ OOPS/third.cpp
@@ -6,7 +6,8 @@ using namespace std;
 class Customer{
     string name;
     int acc_numer, balance;
-    static int totalCustomer;   // Static data member
+    static int totalCustomer;   // Static data member -> to have the cout of totalCustormers
+    static int totalBalance;    //To have the count of totalBalance
 
     public:
 
@@ -15,13 +16,15 @@ class Customer{
         this->acc_numer = acc_numer;
         this->balance  = balance;
         totalCustomer++;
+        totalBalance+=balance;
     }
 
     void displayValue() {
         cout<<name<<" "
             <<acc_numer<<" "
             <<balance<<" "
-            <<totalCustomer<<endl;
+            <<totalCustomer<<" " 
+            <<totalBalance<<endl;
     }
 
     static void access_static_data() {        //Static member function
@@ -30,12 +33,13 @@ class Customer{
 };
 
 int Customer::totalCustomer = 0;    //Initialization of the static data member
+int Customer::totalBalance  = 0;
 
 int main()
 {
     Customer C1("simee", 18, 0);
     C1.displayValue();
-    Customer C2("Anshu", 78, 0);
+    Customer C2("Anshu", 78, 10);
     C2.displayValue();
     Customer::access_static_data();     //Static menmber function helps to access data using Class
 }
