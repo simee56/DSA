10e39d76ae1cbe9058bc46a1be05d917c4f212a3
simee56 <pandeysimee7@gmail.com>
2026-07-03 21:56:57 +0530

    feat: add file read functionality using ifstream

--- OOPS/fileHandling.cpp
+++ OOPS/fileHandling.cpp
@@ -6,14 +6,37 @@
 using namespace std;
 
 int main() {
-    ofstream fout;
+    ofstream fout;    // fout is an object
     // open the file
     fout.open("zoom.txt");
 
-    fout<<"Hey this is sime pandey.";
+    fout<<"Hey this is simee pandey.";
 
     // Close the file
     fout.close();
+}
 
+//Read a file
 
+#include<iostream>
+#include<fstream>
+
+using namespace std;
+
+int main() {
+    ifstream fin;    //fin is an object
+
+    // open the file
+    fin.open("zoom.txt");
+
+    char c;
+    c = fin.get();
+
+    while(!fin.eof()) {
+        cout<<c;
+        c = fin.get();     // Read next character
+    }
+
+    // Close the file
+    fin.close();
 }
