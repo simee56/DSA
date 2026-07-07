6002f95b43d248934db270c1c159f964d4ee93d4
simee56 <pandeysimee7@gmail.com>
2026-07-07 11:43:52 +0530

    Inheritence

--- OOPS/Four_Pillers/Inheritance.cpp
+++ OOPS/Four_Pillers/Inheritance.cpp
@@ -21,3 +21,62 @@ int main() {
   cout << myCar.brand + " " + myCar.model;
   return 0;
 }
+
+
+// Multilevel Inheritance
+// A class can also be derived from one class, which is already derived from another class.
+
+// Base class (parent)
+class MyClass {
+  public:
+    void myFunction() {
+      cout << "Some content in parent class." ;
+    }
+};
+
+// Derived class (child)
+class MyChild: public MyClass {
+};
+
+// Derived class (grandchild)
+class MyGrandChild: public MyChild {
+};
+
+int main() {
+  MyGrandChild myObj;
+  myObj.myFunction();
+  return 0;
+}
+
+
+// // Multiple Inheritance
+// A class can also be derived from more than one base class, using a comma-separated list
+
+// Base class
+class MyClass {
+  public:
+    void myFunction() {
+      cout << "Some content in parent class." ;
+    }
+};
+
+// Another base class
+class MyOtherClass {
+  public:
+    void myOtherFunction() {
+      cout << "Some content in another class." ;
+    }
+};
+
+// Derived class
+class MyChildClass: public MyClass, public MyOtherClass {
+};
+
+int main() {
+  MyChildClass myObj;
+  myObj.myFunction();
+  myObj.myOtherFunction();
+  return 0;
+}
+
+
