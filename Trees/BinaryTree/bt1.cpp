546c8cd3cb1bcb4a4a52788f3e284a890d31d24c
simee56 <pandeysimee7@gmail.com>
2026-07-31 22:23:01 +0530

    Creation of binary tree - Pre- order construction

--- Trees/BinaryTree/bt1.cpp
+++ Trees/BinaryTree/bt1.cpp
@@ -58,3 +58,45 @@ int main()
 
 
 // 2. Pre-order construction
+
+#include<iostream>
+#include<queue>
+using namespace std;
+
+class Node {
+public:
+    int data;
+    Node *left, *right;
+
+    Node(int value) {
+        data = value;
+        left = right = NULL;
+    }
+};
+
+// Function to create nodes 
+Node *BinaryTree()
+{
+    int x;
+    cin>>x;
+
+    if(x == -1)
+    return NULL;
+
+    Node *current = new Node(x);
+
+    // Create the lest child
+    cout << "Enter the left child of " << current->data <<" :";
+    current->left = BinaryTree();
+
+    // Create the right child
+    cout << "Enter the right child of " << current->data  <<" :";
+    current->right =  BinaryTree();
+    return current;
+}
+
+int main()
+{
+    cout<<"Enter the root node : ";
+    Node *root = BinaryTree();
+}
