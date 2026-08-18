a3161d26ebfcf079df26c1c4958dd816c6395fbe
simee56 <pandeysimee7@gmail.com>
2026-08-18 12:05:19 +0530

    feat: implement undirected unweighted graph using adjacency matrix

---
+++ Graph/Representation/Adjaceny_Matrix/adj_mat.cpp
@@ -0,0 +1,34 @@
+// Representation of Graph using adjaceny matrix
+// Undirected unweighted graph
+
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int main()
+{
+    int vertex, edges;
+    cout << "Enter the value of vertices : ";
+    cin >> vertex;
+    cout << "Enter the value of edges : ";
+    cin >> edges; 
+    cout << endl;
+
+    vector<vector<bool>> AdjMat(vertex, vector<bool>(vertex, 0));
+
+    int u, v;
+    cout << "Explain how the edges are going to connect : ";
+    for(int i = 0; i < edges; i++) {
+        cin >> u >> v;
+        AdjMat[u][v] = 1;
+        AdjMat[v][u] = 1;
+    }
+
+    cout << "\nGraph using Adjaceny Matrix is : ";
+    for(int i = 0; i < vertex; i++) {
+        for(int j = 0; j < vertex; j++) 
+            cout << AdjMat[i][j] << " ";
+
+        cout << endl;
+    }
+}
