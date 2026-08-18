2bd63dc5819b4bba5d7a6b39a9875f92cc98cdd6
simee56 <pandeysimee7@gmail.com>
2026-08-18 13:05:47 +0530

    feat: implement undirected weighted graph using adjacency list

---
+++ Graph/Representation/Adjaceny_List/adj_list2.cpp
@@ -0,0 +1,35 @@
+// Representation of a graph using Adjaceny List
+// Undirected Weighted graph
+
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int main()
+{
+    int vertex, edges;
+    cout << "Enter the value of vertices : ";
+    cin >> vertex;
+    cout << "Enter the value of edges : ";
+    cin >> edges; 
+    cout << endl;
+
+    // Creating adjaceny list
+    vector<pair<int, int>> AdjList[vertex];
+
+    int u, v, weight;
+    cout << "Explain how the edges are going to connect : ";
+    for(int i = 0; i < edges; i++) {
+        cin >> u >> v >> weight;
+        AdjList[u].push_back(make_pair(v, weight));
+        AdjList[v].push_back(make_pair(u, weight));
+    }
+
+    for(int i = 0; i < vertex; i++) {
+        cout << i << " -> ";
+        for(int j = 0; j < AdjList[i].size(); j++)
+        cout << AdjList[i][j].first << " " << AdjList[i][j].second << " ";
+
+        cout << endl;
+    }
+}
